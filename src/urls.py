76d7dfe0822abb76d9b9698dd026e362ea42595a
SOMEWELL <melkuman147@gmail.com>
2025-08-09 17:55:15 +0400

    html 21.2

---
+++ src/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+from .views import show_item
+
+urlpatterns = [
+    path('show_data/', views.show_data, name='show_data'),
+    path('submit_data/', views.submit_data, name='submit_data'),
+    path('item/<int:item_id>/',views.show_item, name='show_item')
+]
