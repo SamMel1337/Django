93af805ca110bd2c6b0cc360d3790e777a41d224
SOMEWELL <melkuman147@gmail.com>
2025-10-18 19:26:46 +0400

    html новое CBV

--- users/views.py
+++ users/views.py
@@ -1,50 +1,38 @@
-# Create your views here.
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseForbidden
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import login, logout
+from django.views.generic.edit import FormView
+from django.contrib.auth import login
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.conf import settings
 from .forms import UserRegisterForm, UserLoginForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+
+class RegisterView(FormView):
+    template_name = 'users/register.html'
+    form_class = UserRegisterForm
+    success_url = '/catalog/'  # или используйте reverse_lazy('название_пути')
+
+    def form_valid(self, form):
+        user = form.save()
+        # Отправка приветственного письма
+        send_mail(
+            'Добро пожаловать!',
+            'Спасибо за регистрацию в нашем магазине.',
+            settings.DEFAULT_FROM_EMAIL,
+            [user.email],
+            fail_silently=False,
+        )
+        messages.success(self.request, 'Вы успешно зарегистрированы!')
+        login(self.request, user)
+        return super().form_valid(form)
 
 
-
-def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST, request.FILES)
-        if form.is_valid():
-            user = form.save()
-
-            # Отправка приветственного письма
-            send_mail(
-                'Добро пожаловать!',
-                'Спасибо за регистрацию в нашем магазине.',
-                settings.DEFAULT_FROM_EMAIL,
-                [user.email],
-                fail_silently=False,
-            )
-
-            messages.success(request, 'Вы успешно зарегистрированы!')
-            login(request, user)
-            return redirect('catalog:product_list')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+class CustomLoginView(LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm  # если у вас есть кастомная форма входа
+    redirect_authenticated_user = True
 
 
-def user_login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('catalog:product_list')
-    else:
-        form = UserLoginForm()
-    return render(request, 'users/login.html', {'form': form})
+class CustomLogoutView(LogoutView):
+    next_page = '/catalog/'
 
-
-def user_logout(request):
-    logout(request)
-    return redirect('catalog:product_list')
